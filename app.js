419530a68606296bcbef0102f3a89c0ed71a5f69
Jared Ridabock <jaredridabock@gmail.com>
2021-03-24 14:02:10 -0700

    Added a name and color button

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ const tagDisplay = document.getElementById("tag-id");
 changeButton.addEventListener('click', () => {
     tagDisplay.textContent = tagInput.value;
 });
+
+const colorButton = document.getElementById("color-button");
+const h1Color = document.getElementById("top-box");
+const h2Color = document.getElementById("top-mid-box");
+const pColor = document.getElementById("bottom-box");
+
+colorButton.addEventListener('click', () => {
+    h1Color.style.backgroundColor = 'lightblue';
+    pColor.style.backgroundColor = 'lightblue';
+    h2Color.style.backgroundColor = 'lightblue';
+
+});
